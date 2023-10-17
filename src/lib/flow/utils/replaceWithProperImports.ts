4728be2f43a36772e5987e0e4948a5a1c47a1444
jacob-tucker <jacobtucker818@gmail.com>
2023-10-17 13:43:00 -0400

    better replace with proper imports

--- src/lib/flow/utils/replaceWithProperImports.ts
+++ src/lib/flow/utils/replaceWithProperImports.ts
@@ -1,12 +1,24 @@
 import flowJson from "../../../../flow.json";
 import { network } from "../config";
 
-const replaceWithProperImports = (cadence: string) => {
-  return cadence
-    .replace('"../contracts/utility/NonFungibleToken.cdc"', flowJson.contracts.NonFungibleToken.aliases[network])
-    .replace('"../contracts/utility/FungibleToken.cdc"', flowJson.contracts.FungibleToken.aliases[network])
-    .replace('"../contracts/utility/MetadataViews.cdc"', flowJson.contracts.MetadataViews.aliases[network])
-    .replace('"../contracts/utility/FlowToken.cdc"', flowJson.contracts.FlowToken.aliases[network])
+function replaceWithProperImports(cadence: String) {
+  let broken = cadence.split(/\s/g);
+  for (let i = 0; i < broken.length; i++) {
+    if (broken[i] == "import" && broken[i + 2] == "from") {
+      console.log('Contract Import Replace', broken[i + 1])
+      let contractAddress = flowJson.contracts[broken[i + 1]].aliases[network];
+      if (!contractAddress.startsWith('0x')) {
+        contractAddress = '0x' + contractAddress;
+      }
+      cadence = cadence.replace(broken[i + 3], contractAddress)
+    } else if (
+      broken[i] == "transaction(" ||
+      (broken[i] == "pub" && broken[i + 1] == "fun" && broken[i + 2] == "main")
+    ) {
+      break;
+    }
+  }
+  return cadence;
 }
 
 export default replaceWithProperImports;
